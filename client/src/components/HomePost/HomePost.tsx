151f37bd0c05cbc642f4b38c5cd316b4c594fe58
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2022-08-04 19:50:35 +0300

    update component HomePost.tsx

--- client/src/components/HomePost/HomePost.tsx
+++ client/src/components/HomePost/HomePost.tsx
@@ -1,4 +1,4 @@
-import { IPost } from "../../models/post.models";
+import { IComment, IPost } from "../../models/post.models";
 import classes from "./HomePost.module.sass";
 import { ReactComponent as ThreeDotsIcon } from "../../assets/images/three_dots.svg";
 import { ReactComponent as LikeIcon } from "../../assets/images/heart.svg";
@@ -17,6 +17,37 @@ function HomePost({ info }: { info: IPost }): JSX.Element {
 		avatar: "https://image.api.playstation.com/cdn/EP1965/CUSA05528_00/pImhj205rhCZ3oHbfkKZzvvFL2S3IyL7.png?w=960&h=960"
 	};
 	const [dotsToEnd, setDotsToEnd] = React.useState<boolean>(info.description ? info.description.trim().length >= 200 : false);
+	const [viewComments, setViewComments] = React.useState<boolean>(!!info.usersCommentsIds.length);
+	const [comments, setComments] = React.useState([
+		{
+			id: 1,
+			ownerId: 3,
+			userName: "aalex",
+			text: "So cool",
+			createdAt: "19:36"
+		},
+		{
+			id: 2,
+			ownerId: 4,
+			userName: "otherguy",
+			text: "Neverwinter is cool game",
+			createdAt: "19:03"
+		},
+		{
+			id: 3,
+			ownerId: 53,
+			userName: "quda",
+			text: "Yes",
+			createdAt: "13:36"
+		},
+		{
+			id: 4,
+			ownerId: 43,
+			userName: "qwe]",
+			text: "i write this comment for this post",
+			createdAt: "19:39"
+		}
+	]);
 
 	return (
 		<li className={classes.post}>
@@ -44,45 +75,67 @@ function HomePost({ info }: { info: IPost }): JSX.Element {
 					alt={info.description}
 				/>
 				<div className={classes.bodyDescription}>
-					<div className={classes.bodyDescriptionTop}>
-						<div className={classes.bodyDescriptionBtns}>
-							<button
-								className={cn(classes.bodyDescriptionLike, {
-									[classes.bodyDescriptionLikeActive]: true
-								})}
-							>
-								<LikeIcon />
-							</button>
-						</div>
-						<span className={classes.bodyDescriptionLikesNum}>
-							{info.usersLikesIds.length} likes
-						</span>
-						<div>
-							<p
-								className={cn(classes.bodyDescriptionText, {
-									[classes.bodyDescriptionTextHideSide]: dotsToEnd
-								})}
-							>
-								<span className={classes.headerName}>{userPost.userName}</span>
-								&nbsp;
-								{info.description}
-							</p>
+					<div className={classes.bodyDescriptionBtns}>
+						<button
+							className={cn(classes.bodyDescriptionLike, {
+								[classes.bodyDescriptionLikeActive]: true
+							})}
+						>
+							<LikeIcon />
+						</button>
+					</div>
+					<span className={classes.bodyDescriptionLikesNum}>
+						{info.usersLikesIds.length} likes
+					</span>
+					<div>
+						<p
+							className={cn(classes.bodyDescriptionText, {
+								[classes.bodyDescriptionTextHideSide]: dotsToEnd
+							})}
+						>
+							<span className={classes.headerName}>{userPost.userName}</span>
+							&nbsp;
+							{info.description}
+						</p>
+						<div className={classes.bodyDescriptionActions}>
 							{dotsToEnd && <Button
 								className={classes.bodyDescriptionMore}
 								onClick={() => setDotsToEnd(!dotsToEnd)}
 							>
-								React more
+								Read more
 							</Button>}
-							{dotsToEnd && <Button
+							{viewComments && <Button
 								className={classes.bodyDescriptionMore}
-								onClick={() => setDotsToEnd(!dotsToEnd)}
+								onClick={() => setViewComments(!viewComments)}
 							>
 								View {info.usersCommentsIds.length} comments
 							</Button>}
 						</div>
 					</div>
-					<AddComment className={classes.formComment} />
 				</div>
+				<div className={classes.bodyComments}>
+					<ul className={classes.bodyCommentsList}>
+						{comments.map((_, index: number) => {
+							if (viewComments && index >= 2) return;
+
+							const comment = comments[index];
+
+							return (
+								<li
+									key={comment.id}
+									className={cn(classes.bodyCommentsItem, classes.bodyDescriptionText)}
+								>
+									<span className={classes.headerName}>
+										<Link to={`/profile/${comment.ownerId}`}>{comment.userName}</Link>
+									</span>
+									&nbsp;
+									{comment.text}
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<AddComment className={classes.formComment} />
 			</div>
 		</li>
 	);
