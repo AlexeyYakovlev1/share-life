3356e5b166e2f3659cd3f475bb3692297a8f3497
AlexeyYakovlev1 <alexeyykovlev555@gmail.com>
2022-09-18 19:47:32 +0300

    When upload avatar for update user then removing old avatar.

--- server/controllers/upload.controller.js
+++ server/controllers/upload.controller.js
@@ -2,6 +2,7 @@ const toBase64 = require("../utils/toBase64.util");
 const db = require("../db");
 
 const path = require("path");
+const fs = require("fs");
 
 const { PROJECT_ROOT } = process.env;
 
@@ -10,9 +11,10 @@ class UploadController {
 		const { file } = req;
 
 		if (!file) {
-			return res.status(400).json({ success: false, message: "Avatar is not found" });
+			return res.status(400).json({ success: false, message: "File is not found" });
 		}
 
+		const { id: currentIdUser } = req.user;
 		const pathToFile = path.relative(PROJECT_ROOT, `./templates/user/${file.filename}`);
 		const dataFile = {
 			filename: file.filename,
@@ -23,15 +25,24 @@ class UploadController {
 
 		// update user avatar if query update-user exist
 		if (req.query && updateUser) {
-			const queryForUpdateUser = `UPDATE person SET avatar = $1 WHERE id = $2`;
+			const queryForFindOldAvatar = `SELECT avatar FROM person WHERE id = $1`;
 
-			new Promise((resolve) => resolve(db.query(queryForUpdateUser, [file.filename, req.user.id])))
-				.then(() => {
-					return res.status(200).json({ success: true, message: "Avatar has been uploaded", dataFile });
+			new Promise((resolve) => resolve(db.query(queryForFindOldAvatar, [currentIdUser])))
+				.then((person) => {
+					if (person.rows[0].avatar) {
+						const filePath = path.relative(PROJECT_ROOT, `./templates/user/${person.rows[0].avatar}`);
+
+						fs.unlink(filePath, (err) => {
+							if (err) return Promise.reject(err);
+						});
+					}
+
+					const queryForUpdateUser = `UPDATE person SET avatar = $1 WHERE id = $2`;
+
+					return Promise.resolve(db.query(queryForUpdateUser, [file.filename, currentIdUser]));
 				})
-				.catch((error) => {
-					return res.status(400).json({ success: false, message: error.message, error })
-				});
+				.then(() => res.status(200).json({ success: true, message: "Avatar has been uploaded", dataFile }))
+				.catch((error) => res.status(400).json({ success: false, message: error.message, error }));;
 
 			return;
 		}
